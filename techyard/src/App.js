34bc529cf6b440bc3928bc92c5f3f42bbb447c53
cheveuxdelin <victorba2120@gmail.com>
2022-09-24 21:43:46 -0500

    added material

--- techyard/src/App.js
+++ techyard/src/App.js
@@ -1,8 +1,11 @@
 import Evaluation from "./views/Evaluation";
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline/>
 
       <Evaluation/>
     </div>
