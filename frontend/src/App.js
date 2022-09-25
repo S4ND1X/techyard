70d0a233a08a4ab79cdc29f689e362bbc3578b87
Jorge Sanchez <sandix390@gmail.com>
2022-09-24 22:08:47 -0500

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import Evaluation from "./views/Evaluation";
 import CssBaseline from '@mui/material/CssBaseline';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <CssBaseline/>
 
-      <Evaluation/>
+    <div className="App">
+      <BrowserRouter>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Evaluation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
