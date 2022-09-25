b849219f75c063ffbe4a0b9572ffcc7ad5fc0882
davidxd312 <57538499+davidxd312@users.noreply.github.com>
2022-09-25 01:15:55 -0500

    VICTOR TRABAJA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import Evaluation from "./views/Evaluation";
 import ProductCard from "./components/ProductCard";
 import CssBaseline from '@mui/material/CssBaseline';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import SimplePaper from "./components/Paper"
 
 
 function App() {
   return (
 
     <div className="App">
+      <SimplePaper/>
       <Router>
         <CssBaseline />
         <Routes>
