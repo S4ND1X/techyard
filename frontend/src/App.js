3bd6b1a9dc959b074f474ef976ee7a843eb1620c
Jorge Sanchez <sandix390@gmail.com>
2022-09-24 22:35:13 -0500

    added product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import Evaluation from "./views/Evaluation";
+import ProductCard from "./components/ProductCard";
 import CssBaseline from '@mui/material/CssBaseline';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <CssBaseline/>
 
-      <Evaluation/>
+    <div className="App">
+      <Router>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Evaluation />} />
+          <Route path="/product" element={<ProductCard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
